e5056c4cdc2371c61da382ef0106399dc245bce2
彭淦 <penggan@penggans-MacBook-Air.local>
2021-06-03 16:32:14 +0800

    .

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -8,6 +8,7 @@ export default function MarketingApp() {
 
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
+      initialPath: history.location.pathname,
       onNavigate({ pathname: nextPathname }) {
         const { pathname } = history.location
 
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { createMemoryHistory, createBrowserHistory } from 'history'
 
-function mount(el, { onNavigate, defaultHistory }) {
-  const history = defaultHistory || createMemoryHistory()
+function mount(el, { onNavigate, defaultHistory, initialPath }) {
+  const history = defaultHistory || createMemoryHistory({
+    initialEntries: [initialPath]
+  })
 
   onNavigate && history.listen(onNavigate)
 
